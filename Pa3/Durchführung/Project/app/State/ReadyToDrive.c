661fd901a1e162e6a9914c81eb1b0407e0dbd7f4
Thorben Päpke <t.paepke@cit-hso.de>
2024-05-14 21:07:31 +0200

    implementation ReadyToDrive.c

--- Pa3/Durchführung/Project/app/State/ReadyToDrive.c
+++ Pa3/Durchführung/Project/app/State/ReadyToDrive.c
@@ -28,6 +28,25 @@
 
 EventEnum ReadyToDrive_CheckStateOfButtons(void)
 {
+    EventEnum retEvent = NO_EVENT_HAS_HAPPEND;
 
+    if (BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_A))
+    {
+        retEvent = START_BUTTON_HAS_BEEN_RELEASED;   
+    }
+    else if (BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_B))
+    {
+        retEvent = PARAM_BUTTON_HAS_BEEN_RELEASED;
+    }
+    else if (BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_C))
+    {
+        retEvent = CALIBRATION_BUTTON_HAS_BEEN_RELEASED;  
+    }
+    else 
+    {
+        retEvent = NO_EVENT_HAS_HAPPEND;
+    }
+
+    return retEvent;
 }
 /* INTERNAL FUNCTIONS *****************************************************************************/
