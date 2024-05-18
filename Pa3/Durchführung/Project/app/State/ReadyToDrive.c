a4539d8d6b2b11c7f576ae4ebfdb31679280e35c
Thorben Päpke <t.paepke@cit-hso.de>
2024-05-18 08:30:45 +0200

    added WasButtonPressed function to ReadyToDrive.c

--- Pa3/Durchführung/Project/app/State/ReadyToDrive.c
+++ Pa3/Durchführung/Project/app/State/ReadyToDrive.c
@@ -22,7 +22,11 @@
 
 /* PROTOTYPES *************************************************************************************/
 
+static bool WasButtonPressed(ButtonID);
+
 /* VARIABLES **************************************************************************************/
+
+static ButtonState lastState = BUTTON_STATE_UNKNOWN;
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
@@ -30,23 +34,34 @@ EventEnum ReadyToDrive_CheckStateOfButtons(void)
 {
     EventEnum retEvent = NO_EVENT_HAS_HAPPEND;
 
-    if (BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_A))
+    if (TRUE == WasButtonPressed(BUTTON_ID_A))
     {
-        retEvent = START_BUTTON_HAS_BEEN_RELEASED;   
+        retEvent = START_BUTTON_HAS_BEEN_RELEASED;
     }
-    else if (BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_B))
+    else if (TRUE == WasButtonPressed(BUTTON_ID_B))
     {
         retEvent = PARAM_BUTTON_HAS_BEEN_RELEASED;
     }
-    else if (BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_C))
+    else if (TRUE == WasButtonPressed(BUTTON_ID_C))
     {
         retEvent = CALIBRATION_BUTTON_HAS_BEEN_RELEASED;  
     }
-    else 
-    {
-        retEvent = NO_EVENT_HAS_HAPPEND;
-    }
-
+    
     return retEvent;
 }
 /* INTERNAL FUNCTIONS *****************************************************************************/
+
+bool WasButtonPressed(ButtonID ID)
+{
+    bool retBool = FALSE;
+
+    ButtonState activeState = Button_getState(ID);
+    
+    if (BUTTON_STATE_PRESSED == lastState && BUTTON_STATE_RELEASED == activeState)
+    {
+        retBool = TRUE;
+    }
+    lastState = activeState;
+
+    return retBool;
+}
