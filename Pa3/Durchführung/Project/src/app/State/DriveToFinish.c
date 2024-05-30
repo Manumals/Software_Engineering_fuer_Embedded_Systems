3e2f1f43afdc9af08e87afe1f450d2922ac8fa98
t-paqepke <t.paepke@cit-hso.de>
2024-05-30 20:10:03 +0200

    replaced pointers, magic numbers added timer init

--- Pa3/Durchführung/Project/src/app/State/DriveToFinish.c
+++ Pa3/Durchführung/Project/src/app/State/DriveToFinish.c
@@ -15,6 +15,7 @@
 #include "service/LineSensor.h"
 
 /* CONSTANTS **************************************************************************************/
+#define TRESHOLD_LINE_RECOGNIZED 500
 
 /* MACROS *****************************************************************************************/
 
@@ -30,24 +31,24 @@ EventEnum DriveToFinish_FollowGuideLine(SoftTimer* lapTimer)
 {
     EventEnum retEvent = NO_EVENT_HAS_HAPPEND;
 
-    LineSensorValues* sensorValues;
-    LineSensor_read(sensorValues);
+    LineSensorValues sensorValues;
+    LineSensor_read(&sensorValues);
 
-    DriveHandler_FollowGuideLine(sensorValues);
+    DriveHandler_FollowGuideLine(&sensorValues);
     
     if (SOFTTIMER_IS_EXPIRED(lapTimer))
     {
         retEvent = LAPTIME_IS_TOO_LONG;
     }
-    else if ((500 < sensorValues->value[LINESENSOR_LEFT]) && (500 < sensorValues->value[LINESENSOR_RIGHT]))
+    else if ((TRESHOLD_LINE_RECOGNIZED < sensorValues.value[LINESENSOR_LEFT]) && (TRESHOLD_LINE_RECOGNIZED < sensorValues.value[LINESENSOR_RIGHT]))
     {
         retEvent = START_FINISH_LINE_WAS_RECOGINZED;
     }
-    else if ((500 >= sensorValues->value[LINESENSOR_LEFT]) 
-    && (500 >= sensorValues->value[LINESENSOR_MIDDLE_LEFT]) 
-    && (500 >= sensorValues->value[LINESENSOR_MIDDLE])
-    && (500 >= sensorValues->value[LINESENSOR_MIDDLE_RIGHT])
-    && (500 >= sensorValues->value[LINESENSOR_RIGHT]))
+    else if ((TRESHOLD_LINE_RECOGNIZED >= sensorValues.value[LINESENSOR_LEFT]) 
+    && (TRESHOLD_LINE_RECOGNIZED >= sensorValues.value[LINESENSOR_MIDDLE_LEFT]) 
+    && (TRESHOLD_LINE_RECOGNIZED >= sensorValues.value[LINESENSOR_MIDDLE])
+    && (TRESHOLD_LINE_RECOGNIZED >= sensorValues.value[LINESENSOR_MIDDLE_RIGHT])
+    && (TRESHOLD_LINE_RECOGNIZED >= sensorValues.value[LINESENSOR_RIGHT]))
     {
         retEvent = GUIDELINE_WAS_LOST;
     }
--- Pa3/Durchführung/Project/src/app/State/DriveToStart.c
+++ Pa3/Durchführung/Project/src/app/State/DriveToStart.c
@@ -19,6 +19,8 @@
 #define MAX_START_TIME_s (10U)
 #define MAX_LAP_TIME_s (20U)
 
+#define TRESHOLD_LINE_RECOGNIZED 500
+
 /* MACROS *****************************************************************************************/
 
 /* TYPES ******************************************************************************************/
@@ -26,26 +28,29 @@
 /* PROTOTYPES *************************************************************************************/
 
 /* VARIABLES **************************************************************************************/
-static SoftTimer* gMaxTimeTimer;
-static SoftTimer* gLapTimer;
+static SoftTimer gMaxTimeTimer;
+static SoftTimer gLapTimer;
 
-LineSensorValues* sensorValues;
+LineSensorValues sensorValues;
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
 void DriveToStart_StartMotorsAndTimer(void)
 {
-    SoftTimer_Ret status;
+    SoftTimer_Ret status = SOFTTIMER_RET_UNKNOWNTIMER;
 
-    DriveHandler_FollowGuideLine(sensorValues);
+    LineSensor_read(&sensorValues);
+    SoftTimer_init(&gMaxTimeTimer);
 
-    if (SOFT_TIMER_UNREGISTERED == gMaxTimeTimer->state)
+    DriveHandler_FollowGuideLine(&sensorValues);
+
+    if (SOFT_TIMER_UNREGISTERED == gMaxTimeTimer.state)
     {
-        status = SoftTimerHandler_register(gMaxTimeTimer);
+        status = SoftTimerHandler_register(&gMaxTimeTimer);
     }
     if (SOFTTIMER_RET_SUCCESS == status)
     {
-        SoftTimer_start(gMaxTimeTimer, MAX_START_TIME_s);
+        SoftTimer_start(&gMaxTimeTimer, MAX_START_TIME_s);
     }
 }
 
@@ -53,13 +58,13 @@ EventEnum DriveToStart_FollowGuideLine(void)
 {
     EventEnum retEvent = NO_EVENT_HAS_HAPPEND;
 
-    LineSensor_read(sensorValues);
+    LineSensor_read(&sensorValues);
 
-    if (SOFTTIMER_IS_EXPIRED(gMaxTimeTimer))
+    if (SOFTTIMER_IS_EXPIRED(&gMaxTimeTimer))
     {
         retEvent = DRIVE_TO_START_IS_ACTIVE_FOR_TOO_LONG;
     }
-    else if ((500 < sensorValues->value[LINESENSOR_LEFT]) && (500 < sensorValues->value[LINESENSOR_RIGHT]))
+    else if ((TRESHOLD_LINE_RECOGNIZED < sensorValues.value[LINESENSOR_LEFT]) && (TRESHOLD_LINE_RECOGNIZED < sensorValues.value[LINESENSOR_RIGHT]))
     {
         retEvent = START_FINISH_LINE_WAS_RECOGINZED;
     }
@@ -69,22 +74,26 @@ EventEnum DriveToStart_FollowGuideLine(void)
 
 void DriveToStart_StopTimer(void)
 {
-    SoftTimer_Stop(gMaxTimeTimer);
+    SoftTimer_Stop(&gMaxTimeTimer);
+    SoftTimerHandler_unRegister(&gMaxTimeTimer);
 }
 
 SoftTimer* DriveToStart_StartTimerAndBeep(void)
 {
-    SoftTimer_Ret status;
+    SoftTimer_Ret status = SOFTTIMER_RET_UNKNOWNTIMER;
     SoftTimer* retTimer;
+        
+    SoftTimer_init(retTimer);
+    SoftTimer_init(&gLapTimer);
 
-    if (SOFT_TIMER_UNREGISTERED == gLapTimer->state)
+    if (SOFT_TIMER_UNREGISTERED == gLapTimer.state)
     {
-        status = SoftTimerHandler_register(gLapTimer);
+        status = SoftTimerHandler_register(&gLapTimer);
     }
     if (SOFTTIMER_RET_SUCCESS == status)
     {
-        SoftTimer_start(gLapTimer, MAX_LAP_TIME_s);
-        retTimer = gLapTimer;
+        SoftTimer_start(&gLapTimer, MAX_LAP_TIME_s);
+        retTimer = &gLapTimer;
     }
 
     Buzzer_beep(BUZZER_NOTIFY);
