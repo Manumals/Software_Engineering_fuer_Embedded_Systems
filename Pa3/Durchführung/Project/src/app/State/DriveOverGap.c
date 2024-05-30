6a651c214fc129552e68313bdebe777141441040
mott2 <mott2@stud.hs-offenburg.de>
2024-05-30 17:26:13 +0200

    First implementation Drive Over Gap

--- Pa3/Durchführung/Project/src/app/State/DriveOverGap.c
+++ Pa3/Durchführung/Project/src/app/State/DriveOverGap.c
@@ -15,7 +15,7 @@
 #include "service/LineSensor.h"
 
 /* CONSTANTS **************************************************************************************/
-
+#define STATE_IS_ACTIVE_FOR_TOO_LONG 5000
 /* MACROS *****************************************************************************************/
 
 /* TYPES ******************************************************************************************/
@@ -30,12 +30,32 @@ static UInt16 gEntryTime;
 
 void DriveOverGap_SaveCurrentLapTime(SoftTimer* lapTimer)
 {
-
+    gEntryTime = SoftTimer_get(lapTimer);
 }
 
 EventEnum DriveOverGap_DriveOverGap(SoftTimer* lapTimer)
 {
-  
+    DriveHandler_FindGuideLine();
+
+    EventEnum returnEnum = NO_EVENT_HAS_HAPPEND;
+    LineSensorValues* sensorValues;
+    LineSensor_read(sensorValues);
+
+    for (UInt8 counterSensors = 0; counterSensors < 3; counterSensors++)
+    {   
+        if (500 < sensorValues->value[counterSensors])
+        {
+            returnEnum = THE_GUIDELINE_WAS_RECOGNIZED;
+            counterSensors = 3; /** Stop loop*/
+        }
+    }
+
+    if (STATE_IS_ACTIVE_FOR_TOO_LONG >= (gEntryTime - SoftTimer_get(lapTimer)))
+    {
+        returnEnum = DRIVE_OVER_GAP_IS_ACTIVE_FOR_TOO_LONG;
+    }
+
+    return returnEnum;
 }
 
 /* INTERNAL FUNCTIONS *****************************************************************************/
