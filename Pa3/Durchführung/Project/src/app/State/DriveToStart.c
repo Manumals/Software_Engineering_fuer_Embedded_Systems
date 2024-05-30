05b66a5db1a59f12772699a73c83d93b1d7e2677
t-paqepke <t.paepke@cit-hso.de>
2024-05-30 15:09:11 +0200

    first Implementation of DriveToStart

--- Pa3/Durchführung/Project/src/app/State/DriveToStart.c
+++ Pa3/Durchführung/Project/src/app/State/DriveToStart.c
@@ -16,6 +16,8 @@
 #include "service/Buzzer.h"
 
 /* CONSTANTS **************************************************************************************/
+#define MAX_START_TIME_s (10U)
+#define MAX_LAP_TIME_s (20U)
 
 /* MACROS *****************************************************************************************/
 
@@ -25,27 +27,65 @@
 
 /* VARIABLES **************************************************************************************/
 static SoftTimer* gMaxTimeTimer;
+static SoftTimer* gLapTimer;
+
+LineSensorValues* sensorValues;
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
 void DriveToStart_StartMotorsAndTimer(void)
 {
+    SoftTimer_Ret status;
 
+    DriveHandler_FollowGuideLine(sensorValues);
+
+    if (SOFT_TIMER_UNREGISTERED == gMaxTimeTimer->state)
+    {
+        status = SoftTimerHandler_register(gMaxTimeTimer);
+    }
+    if (SOFTTIMER_RET_SUCCESS == status)
+    {
+        SoftTimer_start(gMaxTimeTimer, MAX_START_TIME_s);
+    }
 }
 
 EventEnum DriveToStart_FollowGuideLine(void)
 {
+    EventEnum retEvent = NO_EVENT_HAS_HAPPEND;
 
+    LineSensor_read(sensorValues);
+
+    if (SOFTTIMER_IS_EXPIRED(gMaxTimeTimer))
+    {
+        retEvent = DRIVE_TO_START_IS_ACTIVE_FOR_TOO_LONG;
+    }
+    else if ((500 < sensorValues->value[LINESENSOR_LEFT]) && (500 < sensorValues->value[LINESENSOR_RIGHT]))
+    {
+        retEvent = START_FINISH_LINE_WAS_RECOGINZED;
+    }
+
+    return retEvent;
 }
 
 void DriveToStart_StopTimer(void)
 {
-
+    SoftTimer_Stop(gMaxTimeTimer);
 }
 
 SoftTimer* DriveToStart_StartTimerAndBeep(void)
 {
-  
+    SoftTimer_Ret status;
+
+    if (SOFT_TIMER_UNREGISTERED == gLapTimer->state)
+    {
+        status = SoftTimerHandler_register(gLapTimer);
+    }
+    if (SOFTTIMER_RET_SUCCESS == status)
+    {
+        SoftTimer_start(gLapTimer, MAX_LAP_TIME_s);
+    }
+
+    Buzzer_beep(BUZZER_NOTIFY);
 }
 
 /* INTERNAL FUNCTIONS *****************************************************************************/
