451a95c5c5381e20f9c84b1126c0b2926f4fb1a6
t-paqepke <t.paepke@cit-hso.de>
2024-05-30 15:49:51 +0200

    first implementation DriveToFinish

--- Pa3/Durchführung/Project/src/app/State/DriveToFinish.c
+++ Pa3/Durchführung/Project/src/app/State/DriveToFinish.c
@@ -26,9 +26,33 @@
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
-EventEnum DriveToFinish_FollowGuideLine(SoftTimer* lapTimer)
+EventEnum DriveToFinish_followGuideLine(SoftTimer* lapTimer)
 {
-  
+    EventEnum retEvent = NO_EVENT_HAS_HAPPEND;
+
+    LineSensorValues* sensorValues;
+    LineSensor_read(sensorValues);
+
+    DriveHandler_followGuideLine(sensorValues);
+    
+    if (SOFTTIMER_IS_EXPIRED(lapTimer))
+    {
+        retEvent = LAPTIME_IS_TOO_LONG;
+    }
+    else if ((500 < sensorValues->value[LINESENSOR_LEFT]) && (500 < sensorValues->value[LINESENSOR_RIGHT]))
+    {
+        retEvent = START_FINISH_LINE_WAS_RECOGINZED;
+    }
+    else if ((500 >= sensorValues->value[LINESENSOR_LEFT]) 
+    && (500 >= sensorValues->value[LINESENSOR_MIDDLE_LEFT]) 
+    && (500 >= sensorValues->value[LINESENSOR_MIDDLE])
+    && (500 >= sensorValues->value[LINESENSOR_MIDDLE_RIGHT])
+    && (500 >= sensorValues->value[LINESENSOR_RIGHT]))
+    {
+        retEvent = GUIDELINE_WAS_LOST;
+    }
+
+    return retEvent;
 }
 
 /* INTERNAL FUNCTIONS *****************************************************************************/
